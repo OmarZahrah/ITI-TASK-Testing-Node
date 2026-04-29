b78103ecb39760fcbec6f465cff8f731f958775e
Salsabeel <58127260+sals1995@users.noreply.github.com>
2026-04-30 00:01:15 +0300

    updating controllers

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,92 +1,67 @@
 const todosModel = require("../models/todo");
-const  APIError   = require("../utilities/errors");
+const APIError = require("../utilities/errors");
 
-const getAllTodos = async (_req, res, next) => {
-  try {
-    const todos = await todosModel.find();
-    res.status(200).json({ data: todos });
-  } catch (err) {
-    next(err);
-  }
+const getAllTodos = async (_req, res, _next) => {
+  const todos = await todosModel.find();
+  res.status(200).json({ data: todos });
 };
 
-const saveTodo = async (req, res, next) => {
+const saveTodo = async (req, res, _next) => {
   const title = req.body.title;
-  try {
-    const newTodo = await await todosModel.create({
-      title: title,
-      userId: req.id,
-    });
-    res.status(201).json({ data: newTodo });
-  } catch (err) {
-    next(err);
-  }
+
+  const newTodo = await await todosModel.create({
+    title: title,
+    userId: req.id,
+  });
+  res.status(201).json({ data: newTodo });
 };
 
-const getTodoById = async (req, res, next) => {
+const getTodoById = async (req, res, _next) => {
   const { id } = req.params;
-  try {
-    const todo = await todosModel.findOne({ _id: id });
-    if (!todo) {
-      throw new APIError(404, "there is no todo with this id");
-    } else {
-      res.status(200).json({ data: todo });
-    }
-  } catch (err) {
-    next(err);
+
+  const todo = await todosModel.findOne({ _id: id });
+  if (!todo) {
+    throw new APIError(404, "there is no todo with this id");
+  } else {
+    res.status(200).json({ data: todo });
   }
 };
 
 /* -------------------- lab ------------------- */
 
 /** update todo by id **/
-const updateTitleTodoById = async (req, res, next) => {
+const updateTitleTodoById = async (req, res, _next) => {
   const { title } = req.body;
   const { id } = req.params;
 
-  try {
-    if (!id || !title) {
-      throw new APIError(400, "must provide title and id to edit todo");
-    } else {
-      const UpdatedTodo = await todosModel.findOneAndUpdate(
-        { _id: id },
-        { title },
-        { new: true },
-      );
-      res.status(200).json({ data: UpdatedTodo });
-    }
-  } catch (e) {
-    next(e);
+  if (!id || !title) {
+    throw new APIError(400, "must provide title and id to edit todo");
+  } else {
+    const UpdatedTodo = await todosModel.findOneAndUpdate(
+      { _id: id },
+      { title },
+      { new: true },
+    );
+    res.status(200).json({ data: UpdatedTodo });
   }
 };
 
 /** get all todos for the logged user=> by id in token **/
-const getUserTodos = async (req, res, next) => {
-  try {
-    const todos = await todosModel.find({ userId: req.id });
-    res
-      .status(200)
-      .json(
-        todos.length != 0
-          ? { data: todos }
-          : { message: "Couldn't find any todos for this user" },
-      );
-  } catch (e) {
-    next(e);
-  }
+const getUserTodos = async (req, res, _next) => {
+  const todos = await todosModel.find({ userId: req.id });
+  res
+    .status(200)
+    .json(
+      todos.length != 0
+        ? { data: todos }
+        : { message: "Couldn't find any todos for this user" },
+    );
 };
 
-
-
-const deleteAllTodos = async(_req,res,next)=>{
-  try {
-    await todosModel.deleteMany();
-    res.status(200).json({ message: "todos have been deleted successfully" })
-  } catch (e) {
-    next(e)    
-  }
-}
-
+const deleteAllTodos = async (_req, res, _next) => {
+  await todosModel.deleteMany();
+  res.status(200).json({ message: "todos have been deleted successfully" });
+};
 
 module.exports = {
   getAllTodos,
